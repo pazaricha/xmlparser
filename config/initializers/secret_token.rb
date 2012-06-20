fd8504003cce017556497a20fbde29d105cdb2e9
Paz Aricha <paz@kensodev.com>
2012-06-20 18:49:51 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xmlparser::Application.config.secret_token = '9fed6233bf7971d57bdb3b23a9c57a9eb5e9c2b2448869d3799c0f97fa55d9c489041b3e32aa8f1d036052c27534033ffe9be4701c6ef1c82f47773769beeef7'
